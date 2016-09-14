347b7cfdaedf4b6f907486052940bb284bf90058
Artem Shevchenko <ashev08@gmail.com>
2016-09-14 13:59:15 +0200

    Make views and actions new and show, create

--- tutu/app/controllers/routes_controller.rb
+++ tutu/app/controllers/routes_controller.rb
@@ -1,5 +1,24 @@
 class RoutesController < ApplicationController
+  
   def index
     @routes = Route.all
+  end
+
+  def show
+    @route = Route.find(params[:id])
+  end
+
+  def new
+    @route = Route.new
+  end
+
+  def create
+    @route = Route.new(route_params)
+  end
+
+  private
+
+  def route_params
+    params.require(:route).permit(:name)
   end
 end
