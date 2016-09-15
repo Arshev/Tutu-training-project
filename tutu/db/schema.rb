4e15f2ded5793fea3fb00da09f2becb7a4c57c2a
Artem Shevchenko <ashev08@gmail.com>
2016-09-15 16:49:08 +0200

    Make associations route->stations

--- tutu/db/schema.rb
+++ tutu/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915121406) do
+ActiveRecord::Schema.define(version: 20160915125316) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -31,8 +31,10 @@ ActiveRecord::Schema.define(version: 20160915121406) do
 
   create_table "trains", force: :cascade do |t|
     t.string   "number_train"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "current_station_id"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
   end
 
 end
