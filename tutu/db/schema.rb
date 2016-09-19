caaeb45b37c4f1fcc7eceb841e78f8f52d4c11e8
Artem Shevchenko <ashev08@gmail.com>
2016-09-19 20:31:10 +0200

    Create models Ticket and User

--- tutu/db/schema.rb
+++ tutu/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915125316) do
+ActiveRecord::Schema.define(version: 20160919182551) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -29,12 +29,25 @@ ActiveRecord::Schema.define(version: 20160915125316) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.string   "serial_number"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "trains", force: :cascade do |t|
     t.string   "number_train"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "current_station_id"
     t.index ["current_station_id"], name: "index_trains_on_current_station_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
