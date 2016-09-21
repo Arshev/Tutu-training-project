7c6bfbfd8b9ce7d9ffeed9c26662ab05ce79a0af
Artem Shevchenko <ashev08@gmail.com>
2016-09-21 11:07:19 +0200

    Bug fix

--- tutu/app/models/railway_station.rb
+++ tutu/app/models/railway_station.rb
@@ -2,8 +2,8 @@ class RailwayStation < ApplicationRecord
   has_many :trains
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
-  has_many :tickets, foreign_key: :arrival_station_id
-  has_many :tickets, foreign_key: :departure_station_id
+  has_many :arrival_tickets, class_name: 'Ticket', foreign_key: :arrival_station_id
+  has_many :departure_tickets, class_name: 'Ticket', foreign_key: :departure_station_id
 
   validates :title, presence: true
 end
