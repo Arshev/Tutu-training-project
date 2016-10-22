3f97b2a70c9d60a469fd3d4553a10a51170e99cd
Artem Shevchenko <ashev08@gmail.com>
2016-10-22 15:11:54 +0200

    Исправил валидацию и сделал вариант присвоения номера вагона

--- tutu/app/models/wagon.rb
+++ tutu/app/models/wagon.rb
@@ -2,14 +2,15 @@ class Wagon < ApplicationRecord
   belongs_to :train
 
   validates :type_wagon, :quantity_top_place, :quantity_down_place, presence: true
-  validates :set_number, uniqueness: true
+  validates :number_wagon, uniqueness: true
 
-  before_create :set_number
+  before_validation :set_number
 
   private
-
+  
   def set_number
-    x = 0
-    self.number_wagon = x + 1
+    
+    self.number_wagon = Wagon.last.number_wagon + 1
+
   end
 end
