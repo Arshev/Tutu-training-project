ee80f7fdcffa91306e598927774d4d0da738486d
Artem Shevchenko <ashev08@gmail.com>
2016-11-01 14:43:20 +0200

    Сделал сортировку станций

--- tutu/app/models/railway_station.rb
+++ tutu/app/models/railway_station.rb
@@ -6,4 +6,22 @@ class RailwayStation < ApplicationRecord
   has_many :departure_tickets, class_name: 'Ticket', foreign_key: :departure_station_id
 
   validates :title, presence: true
+
+  scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
+
+  def update_position(route, position)
+    station_route = station_route(route)
+    station_route.update(position: position) if station_route
+  end
+
+  def position_in(route)
+    station_route(route).try(:position)
+  end
+
+  protected
+
+  def station_route(route)
+    @station_route ||= railway_stations_routes.where(route: route).first
+  end
+
 end
