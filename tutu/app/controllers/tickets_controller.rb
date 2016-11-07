1caefca650326ed1fea37631f378257dda1dc682
Artem Shevchenko <ashev08@gmail.com>
2016-11-07 20:33:49 +0200

    Добавил страницу билета

--- tutu/app/controllers/tickets_controller.rb
+++ tutu/app/controllers/tickets_controller.rb
@@ -1,10 +1,7 @@
 class TicketsController < ApplicationController
 
   def create
-    @ticket = Ticket.new
-    @start_station_id = params[:start_station_id]
-    @end_station_id = params[:end_station_id]
-    @train = params[:train]
+    @ticket = Ticket.new(train: params[:train], start_station_id: params[:start_station_id], end_station_id: params[:end_station_id])
 
     render :show
   end
