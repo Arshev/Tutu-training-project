10a52e2210efb5b66fd25b3dc11404ddb6925d6d
Artem Shevchenko <ashev08@gmail.com>
2016-11-07 11:13:24 +0200

    Исправил глупую ошибку, сделал вывод номеров поездов

--- tutu/app/models/search.rb
+++ tutu/app/models/search.rb
@@ -1,6 +1,5 @@
 class Search
-  def search_train(start_station_id, end_station_id)
+  def self.search_train(start_station_id, end_station_id)
     Train.includes(route: :railway_stations).where(railway_stations: { id: start_station_id }) & Train.includes(route: :railway_stations).where(railway_stations: { id: end_station_id })
   end
-
 end
