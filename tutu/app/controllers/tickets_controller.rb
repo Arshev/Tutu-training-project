0485d04acd897f94b29f211956c05d177c0b0baf
Artem Shevchenko <ashev08@gmail.com>
2016-11-08 14:26:09 +0200

    Покупка билета

--- tutu/app/controllers/tickets_controller.rb
+++ tutu/app/controllers/tickets_controller.rb
@@ -1,12 +1,28 @@
 class TicketsController < ApplicationController
 
-  def create
-    @ticket = Ticket.new(train: params[:train], start_station_id: params[:start_station_id], end_station_id: params[:end_station_id])
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
-    render :show
+  def create
+    @ticket = Ticket.new(serial_number: :id, train_id: params[:train], departure_station_id: params[:start_station_id], arrival_station_id: params[:end_station_id])
+
+    if @ticket.save
+      render :show
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:serial_number, :departure_station_id, :arrival_station_id, :user_id, :train_id)
   end
 
 end
