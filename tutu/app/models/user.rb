1fb1f49dc2efc36e0e62adc970e8be0825d0865c
Artem Shevchenko <ashev08@gmail.com>
2016-11-10 16:36:55 +0200

    Установил devise, сделал миграцию

--- tutu/app/models/user.rb
+++ tutu/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :trackable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :confirmable, :validatable
   has_many :tickets
 end
