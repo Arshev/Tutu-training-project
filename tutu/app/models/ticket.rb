6fb791ca6237cedd5d61c6b341e9e9d0070ce36a
Artem Shevchenko <ashev08@gmail.com>
2016-11-14 17:33:33 +0200

    Ошибка с пользователем в билете

--- tutu/app/models/ticket.rb
+++ tutu/app/models/ticket.rb
@@ -3,11 +3,23 @@ class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :departure_station, class_name: 'RailwayStation', foreign_key: :departure_station_id
   belongs_to :arrival_station, class_name: 'RailwayStation', foreign_key: :arrival_station_id
+  belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_station_id
+  belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
 
   validates :serial_number, presence: true
   before_validation :set_number, on: :create
 
+  after_create :send_notification
+
+  def route_name
+    "#{start_station.title} - #{end_station.title}"
+  end
+
   private
+
+  def send_notification
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
 
   def set_number
     self.serial_number = ["a".."z", "A".."Z", "0".."9"].map(&:to_a).flatten.sample(8).join
--- tutu/db/schema.rb
+++ tutu/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112102839) do
+ActiveRecord::Schema.define(version: 20161113184116) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number"
@@ -57,6 +57,10 @@ ActiveRecord::Schema.define(version: 20161112102839) do
     t.integer  "train_id"
     t.text     "passport"
     t.text     "user_name"
+    t.integer  "start_station_id"
+    t.integer  "end_station_id"
+    t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
+    t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
   end
 
   create_table "trains", force: :cascade do |t|
@@ -104,6 +108,7 @@ ActiveRecord::Schema.define(version: 20161112102839) do
     t.integer  "side_bottom_seats"
     t.integer  "seating_seats"
     t.string   "type"
+    t.index ["id", "type"], name: "index_wagons_on_id_and_type"
     t.index ["train_id"], name: "index_wagons_on_train_id"
   end
 
